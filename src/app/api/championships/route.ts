fd8c343cd054d1d7d56b5f8153bff2e8b88ff349
Nick Stepanenko <massaalonso@yandex.ru>
2025-09-17 18:33:37 +0200

    Database schema connected

---
+++ src/app/api/championships/route.ts
@@ -0,0 +1,54 @@
+import { prisma } from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET() {
+  const data = await prisma.championships.findMany({
+    select: {
+      id: true,
+      title: true,
+      logo: true,
+      serverName: true,
+      serverPass: true,
+      leagueJoinQr: true,
+      cars: {
+        select: {
+          id: true,
+          carNumber: true,
+          driverId: true,
+          driverFirstName: true,
+          driverLastName: true,
+          teamId: true,
+          teamName: true,
+          teamLogo: true,
+          carImage: true,
+          flagImage: true,
+          championshipId: true,
+          penalty: {
+            select: {
+              id: true,
+              raceId: true,
+              carId: true,
+              points: true,
+            },
+          },
+        },
+      },
+      races: {
+        select: {
+          id: true,
+          name: true,
+          round: true,
+          laps: true,
+          mins: true,
+          trackId: true,
+          trackMap: true,
+          trackLogo: true,
+          raceDateTime: true,
+          tyres: true,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(data);
+}
---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+export type Car = {
+  id: number;
+  carNumber: number;
+  firstName: string;
+  lastName: string;
+  teamName: string;
+  teamLogo: string;
+  carImage: string;
+  flagImage: string;
+  penalty: [PenaltyPoints];
+};
+
+export type Race = {
+  id: number;
+  name: string;
+  round: number;
+  laps: number;
+  mins: number;
+  tyres: [string];
+  trackMap: string;
+  trackLogo: string;
+  raceDateTime: string;
+};
+
+export type Championship = {
+  id: number;
+  title: string;
+  logo: string;
+  serverName: string;
+  serverPass: string;
+  leagueJoinQr: string;
+  cars: [Car];
+  races: [Race];
+};
+
+export type PenaltyPoints = {
+  id: number;
+  raceId: number;
+  carId: number;
+  points: number;
+}
